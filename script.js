7d5ccf5dbb5365287818c82d7509af4d05f6a1de
Luiz Andre <122027228+LuckysS3@users.noreply.github.com>
2023-06-08 21:30:03 -0300

    x

---
+++ script.js
@@ -0,0 +1,12 @@
+function mostrasenha(){
+    var inputPass = document.getElementById('ipassword')
+    var btnpass = document.getElementById('icon')
+
+    if(inputPass.type === 'password'){
+        inputPass.setAttribute('type','text')
+        btnpass.classList.replace('bi-eye','bi-eye-slash')
+    }else{
+        inputPass.setAttribute('type','password')
+        btnpass.classList.replace('bi-eye-slash','bi-eye')
+    }
+}
